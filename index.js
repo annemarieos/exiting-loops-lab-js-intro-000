d7f2ed273d32aad31eed4c85e818f3382ddb5055
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-31 16:22:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function breakOut(array, changeValue, stopValue) {
-  for (let i = 0; i < breakOut.length; i++)
-} if (breakOut[i] === stopValue) {
-  break
+const breakOut = [2, 2, 2, 2, 2, 5, 2 ,2]
+let changeValue = 4
+
+for (let i = 0; i < breakOut.length; i++) {
+  if (breakOut[i] !== 2) {
+changeValue = breakOut[i]
+    break
+  } 
 }
